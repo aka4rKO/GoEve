3273f9ae54a84c4f1477a3d78314d1370c0a3100
Nasif Nuha <nasifnuh@gmail.com>
2019-04-18 12:47:47 +0530

    Home and side nav

---
+++ Nasif/goeve_final/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import { Main } from './src/Route';
+
+export default class App extends Component {
+  render() {
+    return (
+        <Main/>
+    );
+  }
+}
---
+++ Nasif/goeve_final/src/components/Interets.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+  import {View, Text, ScrollView, StyleSheet} from "react-native";  
+  
+  class Interets extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>Interests</Text>
+            </View>
+        );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
+  export default Interets;
