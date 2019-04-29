8d8f49e9015047daf16e51925e8946cfde3691a9
Thinura Laksara <thinuralaksara@gmail.com>
2019-04-30 04:47:41 +0530

    categories request

--- Thinura/server.js
+++ Thinura/server.js
@@ -31,3 +31,12 @@ app.listen(3000, () => {
         console.log("Connected to `" + DATABASE_NAME + "`!");
     });
 });
+
+app.get("/categories", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
