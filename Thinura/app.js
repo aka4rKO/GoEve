b1d0022829f6dd2a1f3a4b932c2e67c4b01e988a
Thinura Laksara <thinuralaksara@gmail.com>
2019-04-30 04:03:00 +0530

    Get categories

--- Thinura/app.js
+++ Thinura/app.js
@@ -3,8 +3,22 @@ const express = require('express');
 const app = express();
 app.use((resq, res, next) => {
     res.status(200).json({
-        message:'Changed branch'
+        message: 'Changed branch'
     });
 })
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://thinura:Thinura_1999@thinura-vxvn1.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true
+});
+client.connect(err => {
+    const collection = client.db("Goeve").collection("event-categories");
+    console.log("Collection " + collection)
+    // perform actions on the collection object
+    client.close();
+});
+
 
 module.exports = app;
