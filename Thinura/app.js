d327638f0592ff57f59e0691ab7e15877c9fcfcb
Thinura Laksara <thinuralaksara@gmail.com>
2019-04-30 04:44:34 +0530

    as

--- Thinura/app.js
+++ Thinura/app.js
@@ -1,32 +1,10 @@
 const express = require('express');
 
 const app = express();
-// app.use((resq, res, next) => {
-//     res.status(200).json({
-//         message: 'Changed branch'
-//     });
-// })
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://thinura:Thinura_1999@thinura-vxvn1.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, {
-    useNewUrlParser: true
-});
-var collection;
-client.connect(err => {
-    collection = client.db("Goeve").collection("event-categories");
-    // perform actions on the collection object
-    client.close();
-});
-
-app.get("/", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
+app.use((resq, res, next) => {
+    res.status(200).json({
+        message:'Changed branch'
     });
-});
+})
 
 module.exports = app;
