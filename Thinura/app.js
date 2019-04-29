1fc222b05f20e4df48f503537d35438b1bd652fa
Thinura Laksara <thinuralaksara@gmail.com>
2019-04-30 04:11:06 +0530

    test mongodb 1

--- Thinura/app.js
+++ Thinura/app.js
@@ -20,10 +20,13 @@ client.connect(err => {
     client.close();
 });
 
-app.use((resq, res, next) => {
-    res.status(200).json({
-        message: collection
+app.get("/", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
     });
-})
+});
 
 module.exports = app;
