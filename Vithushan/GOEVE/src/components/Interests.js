30920d004c6ceeac8fe855ff0320f22f081ec5d3
aka4rKO <akram.arm.7@gmail.com>
2019-05-01 02:00:18 +0530

    categories looped

--- Vithushan/GOEVE/src/components/Interests.js
+++ Vithushan/GOEVE/src/components/Interests.js
@@ -1,93 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, ImageBackground } from 'react-native';
-import CardView from 'react-native-cardview';
-import CircleCheckBox, {LABEL_POSITION} from 'react-native-circle-checkbox';
+import InterestsCards from './InterestsCards';
 
 export default class Interests extends Component {
   render() {
+
+    const eventType = [
+      
+      {
+          key: 1,
+          name: 'Techology',
+          url: require('./pics/Science-512.png'),
+          status: false,
+      },
+      {
+          key: 2,
+          name: 'Dance',
+          url: require('./pics/startup.png'),
+          status: true
+      },
+      {
+          key: 3,
+          name: 'Drama',
+          url: require('./pics/download.png'),
+          status: false
+  
+      },
+      {
+          key: 4,
+          name: 'Fun',
+          url: require('./pics/download.png'),
+          status: false
+  
+      },
+      {
+          key: 5,
+          name: 'Party',
+          url: require('./pics/startup.png'),
+          status: false
+  
+      }
+      
+    ]
+
     return (
-      <SafeAreaView style={styles.safeAreaView}>
-        <View style={styles.container}>
-            <View flexDirection="row">
-                <CardView
-                    cardElevation={4}
-                    cardMaxElevation={4}
-                    cornerRadius={8}
-                    style={styles.card}
-                >
-                    <ImageBackground source={require('./pics/startup.png')} style={styles.containerImg} resizeMode={'cover'}>
-                    <View style={styles.check}>
-                    <CircleCheckBox
-                        checked={false}
-                        onToggle={(checked) => console.log('My state is: ', checked)}
-                        outerColor="#4169e1"
-                        filterColor="#dcdcdc"
-                        innerColor="#4169e1"
-                        outerSize={19}
-                        innerSize={10}
-                        filterSize={30}
-                    />
-                    </View>
-                    <Text style={styles.text}>Elevation 2</Text>
-                    </ImageBackground>
-                </CardView>
-                <CardView
-                    cardElevation={4}
-                    cardMaxElevation={4}
-                    cornerRadius={8}
-                    style={styles.card}
-                >
-                    <ImageBackground source={require('./pics/startup.png')} style={styles.containerImg} resizeMode={'cover'}>
-                    <View style={styles.check}>
-                    <CircleCheckBox
-                        checked={true}
-                        onToggle={(checked) => console.log('My state is: ', checked)}
-                        outerColor="#4169e1"
-                        filterColor="#dcdcdc"
-                        innerColor="#4169e1"
-                        outerSize={19}
-                        innerSize={10}
-                        filterSize={30}
-                    />
-                    </View>
-                    <Text style={styles.text}>Elevation 2</Text>
-                    </ImageBackground>
-                </CardView>
-            </View>
-        </View>
-      </SafeAreaView>
+      <InterestsCards data={eventType}/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-  },
-  containerImg: {
-    flex: 1,
-    backgroundColor: '#eee',
-  },
-  card: {
-    backgroundColor: 'white',
-    flex: 1,
-    margin: 5,
-    height: 110
-  },
-  text: {
-      color: "#fff",
-      textAlign: 'center',
-      textAlignVertical: "center",
-      marginTop: 17,
-      fontWeight: 'bold',
-      fontSize: 18
-  },
-  check: {
-      marginTop: 5,
-      marginLeft: 5
-  }
-});
--- Vithushan/GOEVE/src/components/InterestsCards.js
+++ Vithushan/GOEVE/src/components/InterestsCards.js
@@ -1,236 +1,151 @@
-import React, { Component } from 'react';
-import {
-        View,
-        Text,
-        CheckBox,
-        // import { TouchableOpacity } from 'react-native-gesture-handler';
-        Image,
-        ScrollView,
-        StyleSheet,
-        Touchable, 
-        Header,
-        Button, 
-        TouchableOpacity,
-        FlatList
-        } from 'react-native';
-// import { FlatList } from 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, ImageBackground, CheckBox } from 'react-native';
+import CircleCheckBox from 'react-native-circle-checkbox';
+import CardView from 'react-native-cardview';
 
-       
 
-export class InterestsCards extends Component {
+const formatData = (data, numColumns) => {
+    const numberOfFullRows = Math.floor(data.length / numColumns);
 
-    constructor(props){
-        super(props);
-
+    let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
+    while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
+        data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+        numberOfElementsLastRow++;
     }
 
+    return data;
+};
 
+const numColumns = 2;
+export default class App extends React.Component {
+
+    renderItem = ({ item, index }) => {
+        if (item.empty === true) {
+            return (
+                <CardView
+                    cardElevation={0}
+                    cardMaxElevation={0}
+                    cornerRadius={0}
+                    style={styles.itemInvisible}
+                >
+                    <ImageBackground source={item.url} style={styles.containerImg} resizeMode={'cover'}>
+                        <View style={styles.check}>
+                        </View>
+                        <Text style={styles.text}>{item.name}</Text>
+                    </ImageBackground>
+                </CardView>);
+        }
+        return (
+            <CardView
+                cardElevation={4}
+                cardMaxElevation={4}
+                cornerRadius={8}
+                style={styles.card}
+            >
+                <ImageBackground source={item.url} style={styles.containerImg} resizeMode={'cover'}>
+                    <View style={styles.check}>
+                        <CircleCheckBox
+                            checked={false}
+                            onToggle={(checked) => {
+                                console.log('My state is: ', checked);
+                                checked = true;
+                            }}
+                            outerColor="#4169e1"
+                            filterColor="#dcdcdc"
+                            innerColor="#4169e1"
+                            outerSize={19}
+                            innerSize={10}
+                            filterSize={30}
+                        />
+                    </View>
+                    <Text style={styles.text}>{item.name}</Text>
+                </ImageBackground>
+            </CardView>
+        );
+    };
 
     render() {
-       
-
-      
-      const {types} = this.props;
-        let typesList = [];
-        types.forEach((typer,i) => {
-
-
-            checkBoxCheck = () => {
-                
-                 b = typer.type.id;
-
-                //  status=!typer.type.status
-                
-    
-                 alert(b);
-                
-            }   
-    
-            const typeArray = (
-
-                           
-            
-                        <View key={`type${i}`} style={{flex: 2}}>
-                        
-                        <View key={typer.type.id}>
-                    
-
-                         
-                        <TouchableOpacity>
-   
-                            <CheckBox
-                                 value={typer.type.status} 
-                                 onChange={()=>checkBoxCheck.bind(i)}
-                                //onChange={this.props.CheckBoxTest}
-                                style={{height:100,width:200,position:'absolute'}}
-                            />
-                            <Image
-                                source={typer.type.url}
-                                resizeMode={'contain'}
-                                style={{height:100,width:200}}
-                            />
-                           
-                            
-                            </TouchableOpacity>
-                        
-                            
-                        </View>
-                     </View>
-                
-                
-
-                     
-
-                    
-                    
-
-                        /* <View key={`type${i}`} style={{flex: 2}} >
-                         <Text>{typer.type.name}</Text>
-                         <View key={typer.type.id}> 
-                             <TouchableOpacity>
-                             <CheckBox 
-                                 style={{height:100,width:200,position:'absolute'}}
-                             />
-                             <Image
-                                 source={typer.type.url}
-                                 resizeMode={'contain'}
-                                 style={{height:100,width:200}}
-                             />
-                             </TouchableOpacity>
-                         </View>
-                     </View>    */
-                    
-                     
-
-                                  
-
-            )
-
-                
-
-        
-
-            typesList.push(
-                
-                
-                typeArray
-                
-
-               
-                )
-            
-
-            
-
-
-         });
-
-
-        
-
-
-
-         
-         
-        if(typesList.length > 0){
-            return (
-                
-            <ScrollView>
-
-        
-
-        <View   style={styles.header} >
-        <Text style={styles.headerText}>Choose Interest</Text>
-        
-        <View style = {styles.btn} >
-        <Button
-        
-            title="DONE"
-        />
-        </View>
-        </View>
-
-        <View>
-             <Text style={styles.headerText}> Choose at least 3 topics to setup your feed</Text>
-        </View>
-
-
-                {typesList}
-            </ScrollView>
-            )
-        }
-        return(
-       
-            
-            <Text>Empty</Text>
-        )
-
-
-      
+        const { data } = this.props;
+        console.log(data);
+        return (
+            <FlatList
+                data={formatData(data, numColumns)}
+                style={styles.container}
+                renderItem={this.renderItem}
+                numColumns={numColumns}
+            />
+        );
     }
 }
 
+const styles = StyleSheet.create({
 
-  const styles = StyleSheet.create({
-        
-         
-            
-             header: {
-                backgroundColor: '#FF4500',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: 60
-            },
+    header: {
+        backgroundColor: '#FF4500',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 60
+    },
+    headerText: {
+        color: 'black',
+        fontSize: 20,
+        padding: 26,
+        fontWeight: 'bold',
+        alignItems: 'center'
+    },
+    btn: {
+        position: 'absolute',
+        right: 10,
+        top: 15,
+        backgroundColor: '#FF4500',
+        color: 'red'
+    },
+    cb: {
+        height: 100,
+        width: 200,
+        position: 'absolute',
+    },
+    userStyle: {
+        fontSize: 18,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        position: 'absolute'
+    },
+    safeAreaView: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#eee',
+    },
+    containerImg: {
+        flex: 1,
+        backgroundColor: '#eee',
+    },
+    card: {
+        backgroundColor: 'white',
+        flex: 1,
+        margin: 5,
+        height: 110
+    },
+    text: {
+        color: "#000",
+        textAlign: 'center',
+        textAlignVertical: "center",
+        marginTop: 17,
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    check: {
+        marginTop: 5,
+        marginLeft: 5
+    },
+    itemInvisible: {
+        backgroundColor: 'transparent',
+        flex: 1,
+        margin: 5,
+        height: 110
+    }
 
-            headerText: {
-                color:'black',
-                fontSize: 20,
-                padding: 26,
-                fontWeight: 'bold',
-                alignItems: 'center'
-            },
-
-            btn: {    
-                position: 'absolute',
-                right: 10,
-                top: 15,
-                backgroundColor: '#FF4500',        
-                color: 'red'
-            },
-
-            cb: {
-                height: 100,
-                width: 200,
-                position: 'absolute',
-            },
-            userStyle:{
-                fontSize:18,
-                color:'black',
-                fontWeight:'bold',
-                textAlign: 'center',
-                position: 'absolute'
-                
-            }
-
-            // imagee: {
-            //     flex:1,
-            //     flexDirection: 'row'
-            // }
-
-
-
-        });
-
-export default InterestsCards;
-
-// import React from 'react'
-// import InterestsCards from 'InterestsCards'
-
-// export default class Interests extends React.Component {
-//   render() {
-
-//     return (
-      
-//     )
-//   }
-// }
+});
