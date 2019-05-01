d997b7cec2c7b4cfe0097a9df6c2b4b9f70ee660
Amjad <amjadnzr@outlook.com>
2019-05-02 01:16:20 +0530

    added multi rows addition to csv file and the api call

--- Amjad/dataset/AddRating.py
+++ Amjad/dataset/AddRating.py
@@ -19,3 +19,45 @@ def addRate(userId,eventId,rating):
   df.to_csv(r'./dataset/rating.csv', index = False) 
   print("done")
   
+def addManyRate(userIds,eventIds,ratings):
+  # converting the string recieved by the json to a list by splitting 
+  if isinstance(userIds, str) or isinstance(eventIds, str) or isinstance(ratings, str):
+     userIds = userIds.replace('[','').replace(']','').split(',') 
+     eventIds = eventIds.replace('[','').replace(']','').split(',') 
+     ratings = ratings.replace('[','').replace(']','').split(',') 
+     print(len(userIds))
+  # Creating the first Dataframe using dictionary 
+  df1 = pd.read_csv('./dataset/rating.csv')
+  
+  # titles of the csv files
+  col_titles = ('user-id', 'event-id','rating')
+  
+  # stores all data in away to convert to an 3*3 matrix
+  data =[]
+  
+  # algo to fill the data list accordingly
+  for i in range(0,len(userIds)):
+      data.append(userIds[i])
+      data.append(eventIds[i])
+      data.append(ratings[i])
+      
+  # Reshaped data and convert to numpy array
+  rdata = pd.np.array(data).reshape((len(data) // 3, 3)) 
+  
+  df2 =pd.DataFrame(rdata, columns=col_titles)
+  
+  # for appending df2 at the end of df1 
+  df = df1.append(df2, ignore_index = True)
+  
+  # print the merged dataframe
+  print(df)
+
+# converting to a csv file with index = false
+  df.to_csv(r'./dataset/rating.csv', index = False) 
+  print("done")
+
+  
+
+
+   
+    
