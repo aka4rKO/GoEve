abdd82a99980456ebe5e69ce37ca900e7b5e6697
Thinura Laksara <thinuralaksara@gmail.com>
2019-05-04 22:05:33 +0530

    Test env connection

--- Thinura/app.js
+++ Thinura/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use((resq, res, next) => {
+app.use((req, res, next) => {
     res.status(200).json({
         message:'Changed branch'
     });
--- Thinura/server.js
+++ Thinura/server.js
@@ -1,8 +1,5 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-const keys = require('./config/keys')
 
 const app = Express();
 
@@ -11,40 +8,6 @@ app.use(BodyParser.urlencoded({
     extended: true
 }));
 
-var database, collection_user_details, collection_event_details, collection_event_categories, collection_event_rating;
-
-app.listen(3000, () => {
-    MongoClient.connect(keys.mongodb.dbURI, {
-        useNewUrlParser: true
-    }, (error, client) => {
-        if (error) {
-            throw error;
-        }
-        database = client.db(keys.mongodb.db_name);
-        collection_event_details = database.collection(keys.mongodb.db_collection_event_details);
-        collection_event_categories = database.collection(keys.mongodb.db_collection_event_categories);
-        collection_event_rating = database.collection(keys.mongodb.db_collection_event_rating);
-        collection_user_details = database.collection(keys.mongodb.db_collection_user_details);
-        collection_user_tags = database.collection(keys.mongodb.db_collection_user_tags);
-        console.log("Connected to `" + DATABASE_NAME + "`!");
-    });
-});
-
-app.get("/categories", (request, response) => {
-    collection_event_categories.find({}).toArray((error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
-
-app.post("/user", (request, response) => {
-
-    collection_user.insert(request.body, (error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result.result);
-    });
+app.listen(process.env.PORT, () => {
+    console.log("Connection started");
 });
