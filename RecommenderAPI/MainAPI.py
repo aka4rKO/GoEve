a9c719475610cd7d1b7e03b71b10addbf8da599f
aka4rKO <akram.arm.7@gmail.com>
2019-05-04 10:40:56 +0530

    docker done

--- RecommenderAPI/MainAPI.py
+++ RecommenderAPI/MainAPI.py
@@ -152,6 +152,7 @@ def newUser():
 
 # Running the server in localhost:5000    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(debug=True)
 
 
